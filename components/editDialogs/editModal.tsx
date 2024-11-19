dc32b988f14e57848cf232232e061249299c1946
lucas bittencourt rauch <lucas.bittencourtrauch@gmail.com>
2024-11-19 01:50:59 -0300

    feito de edição de produto

--- components/editDialogs/editModal.tsx
+++ components/editDialogs/editModal.tsx
@@ -1,7 +1,8 @@
-import { Assento, GenericData, Sala, TableName } from "@/lib/definitions";
+import { Assento, GenericData, Produto, Sala, TableName } from "@/lib/definitions";
 import EditSala from "./editSala";
 import { ReactElement } from "react";
 import EditAssento from "./editAssento";
+import EditProduto from "./editProduto";
 
 export default function EditModal(props: {
   tableName: TableName;
@@ -14,6 +15,7 @@ export default function EditModal(props: {
   } = {
     salas: <EditSala data={data as Sala} onClose={onClose} />,
     assentos: <EditAssento data={data as Assento} onClose={onClose} />,
+    produtos: <EditProduto data={data as Produto} onClose={onClose} />,
   };
 
   return editModal[tableName];
---
+++ components/editDialogs/editProduto.tsx
@@ -0,0 +1,67 @@
+import { Produto, Sala } from "@/lib/definitions";
+import { FormEvent, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import axios from "axios";
+import { useTableData } from "@/lib/useTableData";
+
+export default function EditProduto(props: { data: Produto; onClose: () => void }) {
+  const { data, onClose } = props;
+  const { editItem } = useTableData();
+  const confirmEdit = (e: FormEvent) => {
+    e.preventDefault();
+    axios
+      .put(
+        "/api/produtos/",
+        {
+          nome: (e as any).target[0].value,
+          preco: (e as any).target[1].value,
+        },
+        {
+          headers: {
+            id: data.id,
+          },
+        }
+      )
+      .then((res) => {
+        editItem(res.data);
+        onClose();
+      })
+      .catch((e) => console.error(e));
+  };
+
+  return (
+    <DialogContent className="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>Editar Produto</DialogTitle>
+      </DialogHeader>
+      <form onSubmit={confirmEdit}>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Nome
+            </Label>
+            <Input defaultValue={data.nome} className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Preço
+            </Label>
+            <Input defaultValue={data.preco} className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Salvar alterações</Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  );
+}
